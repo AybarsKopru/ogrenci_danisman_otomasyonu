6b4fbd936ad48b4621090482463d67098b7be6b8
Aybars Köprü <aybarskpr@gmail.com>
2026-09-25 18:55:01 +0300

    fix: next.config.ts allowedDevOrigins placement

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ['192.168.1.109']
-  }
+  allowedDevOrigins: ['192.168.1.109']
 };
 
 export default nextConfig;
